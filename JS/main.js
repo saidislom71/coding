f3b942f5d2b4ea00665dc72da3e0ff69da713e3f
Saidislom <rsaidislom93@gmail.com>
2025-11-11 11:29:32 +0500

    New folder added

---
+++ JS/main.js
@@ -0,0 +1,101 @@
+let name = "Saidislom"
+console.log(`1-misol: Ismim ${name}. O'zgaruvchining turi "${typeof name}".\n`)
+
+let age = 20
+console.log(`2-misol. Yoshim ${age+5}da.\n`)
+
+const PI = 3.141592653589793
+console.log(`3-misol: PI ning qiymati ${PI} va uning turi "${typeof PI}".\n`)
+
+let a = 60
+let b = 11
+console.log(`4-misol: ${a} va ${b} ning yig'indisi ${a+b} ga teng.\n`)
+
+let message = "Hello world"
+let yosh = 20
+let aniq = true
+let aniqmas 
+let noll = null
+console.log(`5-misol: O'zgaruvchi turlari uchun misollar va ularning turlari:
+         ${message} --> ${typeof message}, 
+         ${yosh} --> ${typeof yosh}, 
+         ${aniq} --> ${typeof aniq},
+         ${aniqmas} --> ${typeof aniqmas},
+         ${noll}--> ${typeof noll}.\n`)
+
+var name1 = "Saidislom"
+name1 = "Ronaldo"
+console.log(`6-misol: Mening ismim ${name1}.\n`)
+
+let TugilganYil = 2005
+console.log(`7-misol: Mening yoshim ${2025-TugilganYil} da.\n`)
+
+let name2 = "Ronaldo"
+let surname = "Cristiano"
+console.log(`8-misol: ${surname} ${name2}.\n`)
+
+let name3 = "Vinicius"
+let age1 = 24
+console.log(`9-misol: Salom men ${name3}, yoshim ${age1}da.\n`)
+
+let error 
+console.log(`10-misol: ${error}.\n`)
+
+let noll1 = null
+console.log(`11-misol: ${noll1}.\n`)
+
+let name4 = "Qurbonbek"
+let age2 = 20
+let isStudent = true
+console.log(`12-misol: ${typeof name4}, ${typeof age2}, ${typeof isStudent}.\n`) 
+
+let text = "Greenfield"
+console.log(`13-misol: ${text}-->${text.length}.\n`)
+
+let matn = "25"
+let son = Number(matn)
+console.log(`14-misol: ${typeof son}.\n`)
+
+let son1 = 100
+let matn1 =  String(son1)
+console.log(`15-misol: ${typeof matn1}.\n`) 
+
+let son2 = 5
+let son3 = son2 * 3
+console.log(`16-misol: ${son3}.\n`)
+
+let matn2 = "Hello "
+let matn3 = "world"
+let matn4 = matn2 + matn3
+console.log(`17-misol: ${matn4}.\n`)
+
+let rang = 17
+rang = "salom"
+console.log(`18-misol: ${rang}.\n`)
+
+let ism = "Ronaldo "
+let familiya = "Cristiano "
+let yosh1 = String(40)
+let matn5 = familiya + ism + yosh1
+console.log(`19-misol: ${matn5}.\n`)
+
+let mantiq = true
+let mantiq1 = false
+console.log(`20-misol: ${mantiq} ${mantiq1}.\n`)
+
+
+console.log(`21-misol: ${typeof null} = ${typeof undefined}.\n`)
+
+console.log(`22-misol: men "JS" ni o'rganayabman.\n`)
+
+let son4 = 0
+console.log(`23-misol: ${++son4}.\n`)
+
+let ism1 = "Abbos", yosh2 = 22, kasb = "Direktor"
+console.log(`24-misol: ${ism1} ${yosh2} ${kasb}.\n`)
+
+let TugilganYil1 = 2000
+let HozirgiYil = 2025
+let yosh3 = HozirgiYil - TugilganYil1
+console.log(`25-misol: ${yosh3}.`)
+
